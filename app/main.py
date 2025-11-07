f9206aa787e78e46a3461e375866c661cadfc655
tdc25 <echeverry2502@gmail.com>
2025-11-06 18:17:52 -0600

    Initial commit

--- app/main.py
+++ app/main.py
@@ -6,8 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello CI/CD"}
-
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hello world"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,9 +8,3 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello CI/CD"}
-
-
-def test_read_2_root():
-    response = client.get("/hello")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello world"}
